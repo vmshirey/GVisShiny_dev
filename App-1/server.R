88a6af1e2df8950b32482d7d1ad52324061ef965
Vaughn Shirey <vms44@drexel.edu>
2016-06-17 14:19:39 -0400

    error fixes within Shiny

--- App-1/server.R
+++ App-1/server.R
@@ -1,6 +1,14 @@
 library(shiny)
+library(googleVis)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
 
+  ## establish connection to data file/database
+  
+  ## query/filter data file based on input parameters
+  
+  ## create and output googleVis Sankey Flow Diagram to user
+  ## plot(gvisSankey(dat, from="source", to="destination", weight="weight"))
+    
 })
--- App-1/ui.R
+++ App-1/ui.R
@@ -1,22 +1,20 @@
 library(shiny)
 
 shinyUI(fluidPage(
-  
   titlePanel("ANSP Entomology Type Sankey Visualization Test"),
-  
   sidebarLayout(
     sidebarPanel(
       helpText("Explore temporal trends within the ANSP 
                Entomology Department Type Collection."),
-      
       sliderInput("range", 
                   label = h3("Year(s) of interest:"),
                   min = 1800, max = 2016, value = c(0, 20), sep = ""),
-    
-      checkboxGroupInput("orders", label = h3("Order(s) of interest:"), 
-                       choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3),
-                       selected = 1),
-      
+      checkboxGroupInput("taxon", label = h3("Order(s) of interest:"), 
+                         choices = list("Coleoptera" = 1, "Diptera" = 2, "Hemiptera" = 3,
+                                        "Hymenoptera" = 4, "Lepidoptera" = 5,
+                                        "Orthoptera" = 6, "Minor Orders" = 7),
+                         selected = 1)
+      ),
       mainPanel()
   )
-)))
+))
